9ed0774bfc2f4426159b9421c4be9b1582ae3fa9
Ryo Shindo <Ryo Shindo>
2016-03-24 00:28:51 +0900

    add template (mock)

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,7 +2,8 @@ import {Component} from 'angular2/core';
 
 @Component({
 	selector: 'my-app',
-	template: '<h1>ToDo</h1>'
+	// template: '<h1>ToDo</h1>'
+	templateUrl: 'app/app.component.html'
 })
 export class AppComponent {
 
