ecde713ba3ad918957dd610887ea28c516284a68
ryo_shindo <ryo_shindo>
2016-03-25 19:27:17 +0900

    帰りたいのでコミット

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core';
+import {Component, Injectable, Pipe, PipeTransform} from 'angular2/core';
 import {NgForm} from 'angular2/common';
 
 import {Todo} from './todo';
@@ -10,9 +10,25 @@ import {Todo} from './todo';
 })
 export class AppComponent {
 	todos: Todo[] = [];
-	newTodo = new Todo("", "");
+	newTodo = new Todo("", false);
 	addTodo() {
 		this.todos.push(this.newTodo);
-		this.newTodo = new Todo("", "");
+		this.newTodo = new Todo("", false);
+	}
+	get size() {
+		return this.todos.length;
+	}
+	get doneSize() {
+		return this.todos.filter( todo => todo.done === true).length;
+	}
+}
+
+@Pipe({
+	name: "currentFilter",
+	pure: false
+})
+export class CurrentFilterPipe implements PipeTransform {
+	transform(item: any[], args: any[]): any {
+		
 	}
 }
